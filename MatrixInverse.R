8ff2239efc45a3271f811e0041b0c06996eeb0d6
Tomasz17 <nie_to@wp.pl>
2015-04-27 00:00:40 +0200

    Update and rename MatrixInverse to MatrixInverse.R
    Final version of MatrixInverse.R

--- MatrixInverse.R
+++ MatrixInverse.R
@@ -1,5 +1,9 @@
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ##set NULL value for inv
   inv <- NULL
+  
+  ##matrix functions
   set <- function(y) {
     x <<- y
     inv <<- NULL
@@ -7,17 +11,25 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setinverse <- function(inverse) inv <<- inverse
   getinverse <- function() inv
-  makeVector<-list(set = set, get = get,
+  
+  ##create output list
+  makeVector<-list(
+       set = set, 
+       get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
+  
+  ##check if inv exists or should be calculated
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
+  
+  ##if doesn't exist - getting matrix and calculating inversion (inv)
   data <- x$get()
   inv <- solve(data, ...)
   x$setinverse(inv)
